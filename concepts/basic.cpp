08480c30821db1a1379b60eb1c8ae77c7b2cedb5
Mohammad Ibrahim <167219967+IbrahimDar786@users.noreply.github.com>
2026-06-05 08:47:31 +0530

    solved 5 problems basic level

--- concepts/basic.cpp
+++ concepts/basic.cpp
@@ -1,50 +1,155 @@
+
+
 #include <iostream>
-#include <map>
 using namespace std;
-
-struct Student {
-  int id;
-  string name;
-  string d_o_b;
-  int age;
-};
-
-int main() {
-  std::map<int, Student> freq;
-  // freq[1] = 33;
-  // freq[2] = 34;
-  // freq[3] = 35;
-  // cout << freq.erase(1);
-  // std::cout << freq[1];
-  Student s1 = {101, "ibrahim", "12-04-2005", 22};
-  Student s2 = {102, "sahil", "12-04-2006", 23};
-  freq[s1.id] = s1;
-  freq[s2.id] = s2;
-
-  // if(freq.find("sahil") != freq.end()){
-  //      freq.erase("sahil");
-  //      cout << "freq size is "<< freq.size()<<endl;
-  //      cout << "data deleted";
-  // }
-
-  // cout <<freq["sahil"].age;
-  // cout <<freq["sahil"].id;
-  // cout <<freq["sahil"].d_o_b;
-
-  map<char, int> marks;
-  marks['D'] = 77;
-  marks['C'] = 88;
-  marks['B'] = 97;
-  marks['A'] = 55;
-
-  for (auto i : marks) {
-    // i.second += 2;
-    if (i.second == 88) {
-      i.second = 60;
+#include <algorithm>
+#include <vector>
+// problem 1: check palindrome
+bool palindrome(string str) {
+  int left = 0;
+  int right = str.length() - 1;
+  bool isPalindrome;
+  while (left < right) {
+    isPalindrome = true;
+    if (str[left] != str[right]) {
+      isPalindrome = false;
+      break;
     }
-    i.second == 77 ? i.second = 50 : i.second;
-    cout << i.first << " " << i.second << endl;
+
+    left++;
+    right--;
   }
 
+  return isPalindrome;
+}
+
+// problem 2: sorting 2nd array
+vector<int> sortingSecArr(vector<int> arr1, vector<int> arr2) {
+  int ptr = 0;
+  for (int i = 0; i < arr2.size(); i++) {
+    for (int j = 0; j < arr1.size(); j++) {
+      if (arr1[j] == arr2[i]) {
+        int temp = arr1[j];
+        arr1[j] = arr1[ptr];
+        arr1[ptr] = temp;
+        ptr++;
+      }
+    }
+  }
+
+  return arr1;
+}
+
+// problem 2: move negative to left and positive to right
+vector<int> moveNegativeToLeft(vector<int> arr) {
+  int left = 0;
+  int right = arr.size() - 1;
+
+  while (left < right) {
+    if (arr[left] < 0) {
+      left++;
+    }
+
+    if (arr[right] > 0) {
+      right--;
+    }
+    if (arr[left] > 0 && arr[right] < 0) {
+      int temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+  return arr;
+}
+
+// problem 3: find missing numbers in array
+void missingNum(vector<int> arr) {
+  int max = INT_MIN;
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  cout << "max : " << max << endl;
+  bool isMissing = true;
+  for (int i = 1; i < max; i++) {
+    isMissing = true;
+    for (int j = 0; j < arr.size(); j++) {
+      if (arr[j] == i) {
+        isMissing = false;
+      }
+    }
+    if (isMissing) {
+      cout << i << " ";
+    }
+  }
+}
+
+// problem 4: find the sub arrays
+void findSubArr(vector<int> arr) {
+  for (int start = 0; start < arr.size(); start++) {
+    for (int end = 0; end < arr.size(); end++) {
+      for (int i = start; i <= end; i++) {
+        cout << arr[i] << " ";
+      }
+      cout << endl;
+    }
+  }
+}
+
+// problem 5: swap numbers Without addition and subtraction
+void swap(int &num1, int &num2) {
+  int temp = num1;
+  num1 = num2;
+  num2 = temp;
+}
+
+int main() {
+  /*
+    string str = "madam";
+  cout << "man starts" << endl;
+  if (palindrome(str)) {
+    cout << str << " is palindrome";
+  } else {
+    cout << str << " is not a palindrome";
+  }
+  */
+
+  /*
+  vector<int> arr1 = {22, 1, 3, 4, 5, 11, 2};
+  vector<int> arr2 = {1, 2, 3, 5};
+  sort(arr1.begin(), arr1.end());
+  for (auto i : sortingSecArr(arr1, arr2)) {
+    cout << i << " ";
+  }
+  */
+
+  /*
+  vector<int> arr = {2,3,-1,0,-2,-22,33,5};
+  arr = moveNegativeToLeft(arr);
+  for(auto i: arr){
+       cout << i << " ";
+  }
+  */
+
+  /*
+   vector<int> arr = {11, 2, 5, 6};
+ missingNum(arr);
+  */
+
+  /*
+  vector<int> arr = {1,2,3};
+  findSubArr(arr);
+  */
+
+  int num1 = 22, num2 = 44;
+   swap(num1, num2);
+   cout << num1 << " "<< num2;
+num1 = 55, num2 = 33;
+swap(num1, num2);
+cout << "\n"<< num1 << " "<< num2<< "\n";
   return 0;
 }
